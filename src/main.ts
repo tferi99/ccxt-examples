b0fcaac15061ee5a9dd7931e49c687371b75345b
tferi99 <toth_ferenc@hotmail.com>
2020-05-09 02:21:18 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,29 @@ import {BasicChart} from './impl/basic-chart';
 import * as ft from './impl/fetch-trades-public';
 import {FetchClosedOrdersHistoryPrivate} from './impl/fetch-closed-orders-history-private';
 import { RateLimitingFetchTest } from './impl/rate-limiting-fetch-test';
+import {Exchange} from "ccxt";
+
+async function createExchange(exchangeId: string, config: any): Promise<Exchange> {
+    return await ExchangeFactory.create('binance', {verbose: false, withMarkets: true});
+}
+
+async function createBinance(config: any): Promise<Exchange> {
+    return await createExchange('binance', config);
+}
 
 async function testExchange() {
-    const exch = await ExchangeFactory.create('binance', {verbose: false, withMarkets: true});
+    const exch = await createBinance({verbose: false, withMarkets: true});
     console.log('Exchange:', exch)
 }
 
 async function balances() {
-    const exch = await ExchangeFactory.create('binance', { withMarkets: true});
+    const exch = await createBinance({verbose: false, withMarkets: true});
+
     bal.getBalance(exch);
 }
 
 async function live_orderbook() {
-    const exch = await ExchangeFactory.create('binance', { withMarkets: true});
+    const exch = await createBinance({verbose: false, withMarkets: true});
     const ob = new LiveOrderbook(exch);
     //ob.printOrderBook('BTC/USDT', 10);
     ob.printOrderBook('MATIC/USDT', 10);
@@ -38,15 +48,15 @@ async function basic_chart() {
 }
 
 async function fetch_trades_public() {
-    const exch = await ExchangeFactory.create('binance', { withMarkets: true});
+    const exch = await createBinance({verbose: false, withMarkets: true});
     const since = exch.milliseconds () - 86400000 // -1 day from now
-    //const symbol = 'MATIC/USDT';
-    const symbol = 'BNB/USDT';
-    ft.fetchTradesPublic(exch, symbol, since, 10);
+    const symbol = 'ADA/USDT';
+    //const symbol = 'BNB/USDT';
+    await ft.fetchTradesPublic(exch, symbol, since, 10);
 }
 
 async function fetch_closed_orders_history_private() {
-    const exch = await ExchangeFactory.create('binance', { withMarkets: true});
+    const exch = await createBinance({verbose: false, withMarkets: true});
     const fco = new FetchClosedOrdersHistoryPrivate();
     const startingDate = '2017-01-01T00:00:00';
     const symbol = 'MATIC/USDT'
@@ -54,7 +64,7 @@ async function fetch_closed_orders_history_private() {
 }
 
 async function rate_limiting_fetch_test() {
-    const exch = await ExchangeFactory.create('binance');
+    const exch = await createBinance({});
     const rlft = new RateLimitingFetchTest();
 
     const concurrent = [
@@ -68,16 +78,17 @@ async function rate_limiting_fetch_test() {
     Promise.all(concurrent);
 }
 //---------------------------------------------------------------------
-async function work() {
+async function doit() {
     //await testExchange();
     //await balances();
     //await live_orderbook();
     //await arbitrage_pairs();
     //await basic_chart();
-    // await fetch_trades_public();
-    await fetch_closed_orders_history_private();
+     await fetch_trades_public();
+    //await fetch_closed_orders_history_private();
     //await rate_limiting_fetch_test();
+
+    console.log('----------------------- END --------------------------');
 }
 
-work();
-
+doit();
